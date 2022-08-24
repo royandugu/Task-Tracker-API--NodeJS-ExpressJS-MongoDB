ff0e6ed1f6bce8fc45e9c1aa2ef7743e097b88d4
myGithub123# <royandugu99009@gmail.com>
2022-08-24 19:28:39 +0545

    TaskController

--- Controllers/taskController.js
+++ Controllers/taskController.js
@@ -1,19 +1,29 @@
+const {StatusCodes}=require("http-status-codes");
+
 const taskModel=require("../Models/taskModel");
-const {getData,getIndvData,postData,updateData,deleteData}=require("./commonOperations");
+const {getAllData,getIndvData,postData,updateData,deleteData}=require("./commonOperations");
 
 const getTasks=async (req,res)=>{
-
+    const result=await getAllData(taskModel);
+    res.status(StatusCodes.OK).json({tasks:result});
 }
 const getIndvTask=async (req,res)=>{
-
+    const {id:taskID}=req.params;
+    const result=await getIndvData(taskModel,taskID);
+    res.status(StatusCodes.OK).json({selectedTask:result});
 }
 const postTask=async (req,res)=>{
-
+    const result=await postData(taskModel,{...req.body});
+    res.status(StatusCodes.OK).json({newTask:result});
 }
 const updateTask=async (req,res)=>{
-
+    const {id:taskID}=req.params;
+    const result=await updateData(taskModel,taskID,{...req.body});
+    res.status(StatusCodes.OK).json({updatedTask:result});
 }
 const deleteTask=async (req,res)=>{
-
+    const {id:taskID}=req.params;
+    const result=await deleteData(taskModel,taskID);
+    res.status(StatusCodes.OK).json({deletedTask:result});
 }
 module.exports={getTasks,getIndvTask,postTask,updateTask,deleteTask};
