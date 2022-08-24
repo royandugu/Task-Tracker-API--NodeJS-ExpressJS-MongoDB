f2f61e213bc1fd6a240d29aa4151779781dd476b
myGithub123# <royandugu99009@gmail.com>
2022-08-24 19:29:15 +0545

    Token checker

---
+++ Middlewares/tokenChecker.js
@@ -0,0 +1,13 @@
+const jwt=require("jsonwebtoken");
+const AuthenticationError=require("../Error_Handlers/authenticationError");
+
+const tokenChecker=()=>{
+    try{
+        //Verify the token created
+    }
+    catch(err){
+        throw new AuthenticationError("The page you are trying to access is restricted");
+    }
+}
+
+module.exports=tokenChecker;
