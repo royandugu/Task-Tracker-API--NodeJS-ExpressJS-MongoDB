bf8a35dd9e6ae06120fb229cea93281a9d42ac57
myGithub123# <royandugu99009@gmail.com>
2022-08-26 20:04:06 +0545

    register/login

---
+++ Controllers/user.js
@@ -0,0 +1,9 @@
+const jwt=require("jsonwebtoken");
+
+const register=(req,res)=>{
+    //Handle user registration
+}
+const login=(req,res)=>{
+    //Handle suer login
+}
+module.exports={login,register};
